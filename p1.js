35f005c57a007d060d629239958c8252aa23fa45
Josh Lapham <josh@joshlapham.com>
2012-01-26 11:27:24 +1100

    Problem 1 almost solved. Need to solve issue of multiples counting twice.

--- p1.js
+++ p1.js
@@ -1,11 +1,18 @@
-var startNumber = 1;
-var endNumber = 10;
+var lastResult = 0;
 
-var findMultiples = function ( n1, n2 ) {
-  var i;
-  for (i = startNumber; i < endNumber; i++) {
-    return n1 * i;
-    return n2 * i;
-  }
+var findMultiples = function ( n1 ) {
+    var startNumber = 1;
+    var endNumber = 10;
+    var result;
+    while ((n1 * startNumber) < endNumber) {
+        result = (n1 * startNumber);
+        startNumber++;
+        lastResult += result;
+        //console.log("Sum is " +lastResult);
+    }
 };
 
+findMultiples(3);
+console.log(lastResult);
+findMultiples(5);
+console.log(lastResult);
