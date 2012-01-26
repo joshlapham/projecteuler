aff774045653079103175019ff74898ce503aebd
Josh Lapham <josh@joshlapham.com>
2012-01-26 11:48:23 +1100

    Solved problem 1 with Javascript and Python. Needed help.

--- p1.js
+++ p1.js
@@ -1,18 +1,8 @@
-var lastResult = 0;
+var total = 0;
 
-var findMultiples = function ( n1 ) {
-    var startNumber = 1;
-    var endNumber = 10;
-    var result;
-    while ((n1 * startNumber) < endNumber) {
-        result = (n1 * startNumber);
-        startNumber++;
-        lastResult += result;
-        //console.log("Sum is " +lastResult);
+for (var i = 0; i < 1000; i++) {
+    if ( i % 3 === 0 || i % 5 === 0 ) {
+        total += i;
+        console.log(total);
     }
-};
-
-findMultiples(3);
-console.log(lastResult);
-findMultiples(5);
-console.log(lastResult);
+}
---
+++ p1.py
@@ -0,0 +1,1 @@
+print sum(set(range(3,1000,3)).union(set(range(5,1000,5))))
